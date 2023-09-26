8bb3e7013add1305b99de9c1b3cd578b4a56d1b4
Pukion <pukionable@gmail.com>
2023-09-26 16:54:01 +0100

    Test

--- 2022/Day 1/Java.java
+++ 2022/Day 1/Java.java
@@ -1,3 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Java {
     
+    public static void main(String[] args) throws Exception{
+
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+
+            List<String> inputs = new ArrayList<>();
+
+            String line;
+            while((line = br.readLine()) != null){
+                inputs.add(line);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void printout(ArrayList<String> x){
+        System.out.println(x);
+    }
 }
