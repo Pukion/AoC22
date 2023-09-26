bd360cc3cc6d0d98dd0b0f821dd7d8167619d4a1
Pukion <pukionable@gmail.com>
2023-09-26 23:30:23 +0100

    Part Two

--- 2022/Day_1/Java.java
+++ 2022/Day_1/Java.java
@@ -5,21 +5,23 @@ import java.util.List;
 
 public class Java {
     
+    public static List<String> inputs = new ArrayList<>();
+    public static String currentString;
+    public static int currentCal = 0;
+    public static int previousCal = 0;
+    public static int biggestCal = 0;
+    public static int mediumCal = 0;
+    public static int smallCal = 0;
+
     public static void main(String[] args) throws Exception{
 
         try{
             BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 
-            List<String> inputs = new ArrayList<>();
-
             String line;
             while((line = br.readLine()) != null){
                 inputs.add(line);
             }
-            String currentString;
-            int biggestCal = 0;
-            int currentCal = 0;
-            int previousCal = 0;
             for(int i = 0; i < inputs.size(); i++){
                 currentString = inputs.get(i);
                 if(!currentString.isEmpty()){
@@ -36,7 +38,44 @@ public class Java {
                     }
                 }   
             }
-        System.out.println(biggestCal);
+            System.out.println(biggestCal);
+            //Part Two 
+            currentCal = 0;
+            previousCal = 0;
+            biggestCal = 0;
+
+            for(int j = 0; j < inputs.size(); j++){
+                currentString = inputs.get(j);
+                if(!currentString.isEmpty()){
+                    currentCal = Integer.valueOf(currentString);
+                    previousCal = previousCal + currentCal;
+                }
+                else{
+                    if(biggestCal < previousCal || mediumCal < previousCal || smallCal < previousCal){
+                        if(biggestCal < mediumCal){
+                            if(biggestCal < smallCal){
+                                biggestCal = previousCal;
+                                previousCal = 0;
+                            }
+                            else{
+                                smallCal = previousCal;
+                                previousCal = 0;
+                            }
+                        }
+                        else if(mediumCal < smallCal){
+                            mediumCal = previousCal;
+                            previousCal = 0;
+                        }
+                        else{
+                            smallCal = previousCal;
+                            previousCal = 0;
+                        }
+                    }
+                    previousCal = 0;
+                }
+            }
+            int result = biggestCal + mediumCal + smallCal;
+            System.out.println(result);
         }
         catch(Exception e){
             e.printStackTrace();
