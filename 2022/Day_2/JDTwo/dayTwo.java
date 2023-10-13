6c7ba05c1d13563961b89784ee3bd3d931c19eaf
Pukion <pukionable@gmail.com>
2023-10-13 16:09:25 +0100

    Test

--- 2022/Day_2/JDTwo/dayTwo.java
+++ 2022/Day_2/JDTwo/dayTwo.java
@@ -1,27 +1,49 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class dayTwo {
 
+    List<String> inputs = new ArrayList<>();
+    HashMap<String, Integer> guide = new HashMap<String, Integer>();
+
     public static void main(String[] args){
 
         dayTwo obj = new dayTwo();
-
+        obj.startMap();
+        
         try{
-            try(BufferedReader br = new BufferedReader(new FileReader("Input2.txt"))) {
-                List<String> inputs = new ArrayList<>();
+            try(BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
 
                 String line;
                 while((line = br.readLine()) != null){
-                    inputs.add(line.replaceAll("\\s", ""));
+                    obj.inputs.add(line.replaceAll("\\s", ""));
                 }
-                System.out.println(inputs);
+                System.out.println(obj.inputs);
             }
         }
         catch(Exception e){
             e.printStackTrace();
         }
     }
-}
+    
+    public void startMap(){
+        guide.put("AX", 4);
+        guide.put("AY", 8);
+        guide.put("AZ", 3);
+        guide.put("BX", 1);
+        guide.put("BY", 5);
+        guide.put("BZ", 9);
+        guide.put("CX", 7);
+        guide.put("CY", 2);
+        guide.put("CZ", 6);
+
+    }
+    public void calculate(){
+        for(int i = 0; i < inputs.size(); i++){
+
+        }
+    }
+}   
